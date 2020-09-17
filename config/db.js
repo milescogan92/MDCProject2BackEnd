5778f8a2311d304aa4d2c74d492fb1dff79d00e7
milescogan92 <miles.cogan92@gmail.com>
2020-09-17 09:30:00 -0400

    Second commit - renamed repo
    renamed back end repository from 'MDCProject2' to 'MDCProject2BackEnd'
    
    replaced previous instances where 'MDCProject2' appeared and changed those to new repo name.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'MDCProject2'
+const mongooseBaseName = 'MDCProject2BackEnd'
 
 // create the mongodb uri for development and test
 const database = {
