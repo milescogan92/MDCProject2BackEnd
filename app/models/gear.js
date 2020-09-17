5ceaedc75bcb43da7ae67091ae6a3f01251066d7
milescogan92 <miles.cogan92@gmail.com>
2020-09-17 09:52:28 -0400

    Created gear.js model
    made model for fishing gear setup, called gearSchema and exported as 'Gear'.
    
    gearSchema takes three options - rod, reel, tackle.
    
    included user as owner.

---
+++ app/models/gear.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const gearSchema = new mongoose.Schema({
+  rod: {
+    type: String,
+    required: true
+  },
+  reel: {
+    type: String,
+    required: true
+  },
+  tackle: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Gear', exampleSchema)
